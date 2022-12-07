225e662b2d26556dbb3c2cb611d3be16cef91ab5
Pretty Pandey <prettypandeypearl@gmail.com>
2022-12-07 08:50:59 -0800

    Task 12 code fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,9 +5,9 @@ def roman_to_int(roman_string):
         return 0
 
     roman_dict = {
-            'I': 1, 'V': 5,
-            'X': 10, 'L': 50,
-            'C': 100, 'D': 500, 'M': 1000}
+        'I': 1, 'V': 5,
+        'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000}
 
     num = 0
     r_digt = 0
