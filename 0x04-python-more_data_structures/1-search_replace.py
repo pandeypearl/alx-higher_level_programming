6c3d0024dd4c9ec2d803b89dfb51130dd56e9878
Pretty Pandey <prettypandeypearl@gmail.com>
2022-12-07 01:58:27 -0800

    Adding Task 1 file

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    if my_list is None:
+        return
+    new = my_list[:]
+    for idx, c in enumerate(new):
+        if c == search:
+            new[idx] = replace
+    return new
