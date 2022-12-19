38c0e0306c4ced9f5bc33fa5a512c30132ac18e8
Pretty Pandey <prettypandeypearl@gmail.com>
2022-12-19 09:53:22 -0800

    fixing bare exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end='')
             p = p + 1
-        except:
+        except TypeError:
             break
 
     print()
--- 0x05-python-exceptions/README.md
+++ 0x05-python-exceptions/README.md
@@ -3,7 +3,7 @@
 TASKS
 0. Safe list printing
 
-1. Safe printing of an integers list 
+1. Safe printing of an integers list
 
 2. Print and count integers
 
