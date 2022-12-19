8e14097cce01717bff6ed4e3d20bc151d43ff016
Pretty Pandey <prettypandeypearl@gmail.com>
2022-12-19 09:59:06 -0800

    code fix Task 0 bare except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,12 +2,15 @@
 
 def safe_print_list(my_list=[], x=0):
     p = 0
-    for i in range(x):
-        try:
-            print("{}".format(my_list[i]), end='')
-            p = p + 1
-        except TypeError:
-            break
 
-    print()
-    return p
+    try:
+        for i in my_list:
+            if p < x:
+                print('{}'.format(my_list[p]), end='')
+                p += 1
+
+        print()
+    except TypeError:
+        pass
+    finally:
+        return p
