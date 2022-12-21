70b172f844654fc410cf8f35ffe0dc0919005348
Pretty Pandey <prettypandeypearl@gmail.com>
2022-12-21 12:48:04 -0800

    Adding advanced task 10 file

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+import math
+
+
+class MagicClass:
+    """Magic class
+
+    Emulatyes python bytecode
+
+    """
+    def __init__(self, radius=0):
+        """init
+
+        getting the radius
+
+        """
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+        else:
+            self.__radius = radius
+
+    def area(self):
+        """Area
+
+        calculates the area
+
+        """
+        return (self.__radius ** 2) * math.
+
+    def circumference(self):
+        """Circumference
+
+        Calculates the circumference
+
+        """
+        return (2 * math.pi) * self.__radius
