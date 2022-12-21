38d9e5cde9af4091bc8f7bcb04d79a6fd1629115
Pretty Pandey <prettypandeypearl@gmail.com>
2022-12-21 07:08:56 -0800

    code fix Task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
-"""Square Class
+""""0-square
 
-This class doesn't do anything at the moment
+This module implements the empty class square for educational purposes
+
+Example:
+    This module can be called from a main caller function like:
+
+    Square = __import__('0-square').Square
+    my_square = Square()
 
 """
 
 
 class Square:
+    """Square Class
+
+    This class is not intended to have anything
+    """
     pass
