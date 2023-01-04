9a4b5661ede5a20c095712f3ca3d1d14ac4e19bc
Pretty Pandey <prettypandeypearl@gmail.com>
2023-01-04 04:55:01 -0800

    code fix

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string(times=[0]):
     times[0] = times[0] + 1
-    return "BestSchool" + (", BestSchool" * (times[0] - 1)
+    return "Holberton" + (", Holberton" * (times[0] - 1)
