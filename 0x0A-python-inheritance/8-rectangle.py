355df40e37d14faf72f0fe10e8633b8d36ee05ae
Pretty Pandey <prettypandeypearl@gmail.com>
2023-01-10 09:15:03 -0800

    adding task 8 file

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""8-rectangle
+
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Class that inherits from BaseGeometry
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
