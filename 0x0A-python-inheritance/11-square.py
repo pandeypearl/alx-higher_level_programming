ff21fb92c092785713c51c2c864ffed6fd2a4a89
Pretty Pandey <prettypandeypearl@gmail.com>
2023-01-10 09:44:25 -0800

    code fix task 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -2,7 +2,6 @@
 """11-square
 
 """
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
 
 
@@ -14,8 +13,10 @@ class Square(Rectangle):
     """
     def __init__(self, size):
         self.integer_validator(size, size)
-        super().__init__(size, size)
         self.__size = size
 
+    def area(self):
+        return self.__size ** 2
+
     def __str__(self):
-        return '[Square] ' + str(self.__size) + '/' + str(self.__size)
+        return "[Square] {}/{}".format(self.__size, self.__size)
