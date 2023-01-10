7e50b7e56428e5fe9b6b051dda8f0c33b0c84800
Pretty Pandey <prettypandeypearl@gmail.com>
2023-01-10 09:21:50 -0800

    adding task 9 files

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""9-rectangle
+
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    class Rectangle that inherits from BaseGeometry (7-base_geometry.py)
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return '[Rectangle] ' + str(self.__width) + '/' + str(self.__height)
