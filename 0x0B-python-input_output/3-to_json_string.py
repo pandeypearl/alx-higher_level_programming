6b4f94adf071a59fd54e1da50685e4a6a235e134
Pretty Pandey <prettypandeypearl@gmail.com>
2023-01-10 11:06:02 -0800

    adding task 3 file

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""3-to-json-string
+
+"""
+from json import
+
+
+def to_json_string(my_obj):
+    """
+    Returns the JSON representation of an object (string)
+    """
+    return dumps(my_obj)
