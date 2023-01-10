6ba5fab0c3e5c8f6eef22a8cb4e1718b8a2a046d
Pretty Pandey <prettypandeypearl@gmail.com>
2023-01-10 12:21:14 -0800

    adding task 13 file

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""100-append_after
+
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    Inserts a line of text to a file, after each line containing
+    a specific string
+    """
+    with open(filename, mode='r', encoding='utf-8') as f:
+        text = f.readlines()
+        new_text = []
+
+        for line in text:
+            new_text.append(line)
+
+            if search_string in line:
+                new_text.append(new_string)
+
+    with open(filename, mode='w', encoding='utf-8') as nf:
+        for line in new_text:
+            nf.write(line)
