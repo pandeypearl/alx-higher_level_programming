126aef9e3a1a85a7cdd9cd96dfb7ef35761d8fab
Pretty Pandey <prettypandeypearl@gmail.com>
2023-01-10 02:18:42 -0800

    adding task 6 file

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""6-base-geometry
+
+"""
+
+
+class BaseGeometry:
+    def area(self):
+        """
+        Public instance method
+
+        Exception:
+            'area() is not implemented'
+            'You are not allowed to import any module'
+        """
+        raise Exception('area() is not implemented')
