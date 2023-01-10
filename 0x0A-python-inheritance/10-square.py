18807c827c463fd839769d1938d183021ae386eb
Pretty Pandey <prettypandeypearl@gmail.com>
2023-01-10 09:27:43 -0800

    adding task 10 file

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""10-square-1
+
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    class Square that inherits from Rectangle (9-rectangle.py)
+    """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
