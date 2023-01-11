0e0494563f8e8978878a39b0d05c0f7affd7a96d
Pretty Pandey <prettypandeypearl@gmail.com>
2023-01-11 01:12:52 -0800

    code fix task 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -12,8 +12,9 @@ class Square(Rectangle):
     Task based on 10-square.py
     """
     def __init__(self, size):
-        self.integer_validator(size, size)
+        self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
 
     def area(self):
         return self.__size ** 2
