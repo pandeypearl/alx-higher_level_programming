a3a3f0e79b40f780170c89543b8adf0b2b84463a
Pretty Pandey <prettypandeypearl@gmail.com>
2023-02-14 05:56:52 -0800

    type fix Task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- Full creation
 -- creates a table second_table in the database and add multiples rows
-CREATE TABLE IF NOT EXISTS second_tabele (
+CREATE TABLE IF NOT EXISTS second_table (
 	id INT,
 	name VARCHAR(256),
 	score INT
