4beb7147de55cd7076818f9e7ba3d3297e1131d0
Pretty Pandey <prettypandeypearl@gmail.com>
2023-03-13 16:36:31 +0000

    Code fix Task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const argv = process.argv;
-if (argv[2] == undefined) {
-  console.log('No Argument');
+if (argv[2] === undefined) {
+  console.log('No argument');
 } else {
   console.log(argv[2]);
 }
