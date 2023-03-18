78a98026c5615f65c9ba9da0e64d2618c03a2bb3
Pretty Pandey <prettypandeypearl@gmail.com>
2023-03-18 08:51:07 -0700

    Code fix Task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,7 @@ import sys
 if __name__ == '__main__':
     db = MySQLdb.connect(
             user=sys.argv[1],
-            passwrd=sysargv[2],
+            passwrd=sys.argv[2],
             db=sys.argv[3],
             port=3306,
             host='localhost')
