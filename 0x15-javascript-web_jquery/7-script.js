6bd6cc4473b2c70cd4d6266748757ad5c0bea419
Pretty Pandey <prettypandeypearl@gmail.com>
2023-05-03 05:27:28 -0700

    Star Wars movies

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,4 +1,4 @@
-$(document).ready(() => {
+$('document').ready(() => {
   $.get('https://swapi-api.alx-tools.com/api/people/5/?format=json',
     (data) => {
       $('DIV#character').text(data.name);
